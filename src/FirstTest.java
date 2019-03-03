551dbd0e35257009940cdfdd7d764c952e9e21ad
Your Name <you@example.com>
2019-03-04 00:56:45 +0300

    ex2

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,11 +1,17 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
+import java.nio.file.WatchEvent;
 
 public class FirstTest {
 
@@ -17,7 +23,7 @@ public class FirstTest {
             DesiredCapabilities capabilities = new DesiredCapabilities();
             capabilities.setCapability("platformName","Android");
             capabilities.setCapability("deviceName","AndroidTestDevice");
-            capabilities.setCapability("platformVersion","6.0");
+            capabilities.setCapability("platformVersion","9");
             capabilities.setCapability("automationName","Appium");
             capabilities.setCapability("appPackage","org.wikipedia");
             capabilities.setCapability("appActivity",".main.MainActivity");
@@ -31,9 +37,82 @@ public class FirstTest {
             driver.quit();
         }
         @Test
-        public void firstTest()
+        public void functionCreation()
         {
-            System.out.println("First test run");
+            waitForElementAndClick(
+
+                    By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                    "Cannot find Search Wikipedia input",
+                    5
+
+            );
+            waitForElementAndSendKeys(
+                    By.xpath("//*[contains(@text,'Search…')]"),
+                    "Java",
+                    "cannot find search input",
+                    5
+            );
+
+
+            waitForElementPresent(
+                    By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='Object-oriented programming language']"),
+                    "Cannot find 'Object-oriented programming language topic searching by Java",
+                    15
+            );
         }
+
+
+
+
+
+
+
+
+        private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+            wait.withMessage(error_message + "\n");
+            return wait.until(
+                    ExpectedConditions.presenceOfElementLocated(by)
+            );
+        }
+        private WebElement waitForElementPresent(By by, String error_message)
+        {
+            return waitForElementPresent(by, error_message, 5);
+        }
+
+        private WebElement waitForElementAndClick(By by, String error_message, long timeoutInSeconds)
+        {
+            WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
+            element.click();
+            return element;
+        }
+         private WebElement waitForElementAndSendKeys(By by, String value, String error_message, long timeoutInSeconds)
+        {
+            WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
+            element.sendKeys(value);
+            return element;
+        }
+
+
+        private boolean waitForElementNotPresent(By by, String error_message, long timeoutInSeconds)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+            wait.withMessage(error_message + "\n");
+            return wait.until(
+                    ExpectedConditions.invisibilityOfElementLocated(by)
+            );
+        }
+        private WebElement waitForElementAndClear(By by, String error_message, long timeoutInSeconds)
+        {
+            WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
+            element.clear();
+            return element;
+        }
+
+
+
+
+
 
 }
